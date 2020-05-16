fedbba2b02bab924462b678f45b649ebb64c8538
lucaschimweg <l.schimweg@gmail.com>
2020-05-16 14:07:05 +0200

    Python: Release version 0.3.0 for Hopper 0.3

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hopper_api",
-    version="0.0.2",
+    version="0.3.0",
     author="The hopper team",
     author_email="info@hoppercloud.net",
     description="Hopper's app API",
